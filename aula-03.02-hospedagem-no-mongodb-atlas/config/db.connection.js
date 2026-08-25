3c5561ffa5b8770d067b66b24280af5aeb366326
Anny Vitoria <annyvihalves3@gmail.com>
2026-08-25 20:21:20 -0300

    Conexão com atlas

---
+++ aula-03.02-hospedagem-no-mongodb-atlas/config/db.connection.js
@@ -0,0 +1,21 @@
+// Importando o mongoose
+import mongoose from "mongoose";
+
+// Usuário e senha do banco de dados
+const dbUser = "annyvihalves3_db_user";
+const dbPassoword = "SM0uaH0JuFuHySOF";
+const connect = () => {
+    mongoose.connect(
+        `mongodb+srv://annyvihalves3_db_user:SM0uaH0JuFuHySOF@cluster0.qxxqdxd.mongodb.net/?appName=Cluster0`
+    );
+    const connection = mongoose.connection;
+    connection.on("error", () => {
+        console.log("Erro ao conectar com o mongoDB.")
+    });
+    connection.on("open", () =>{
+        console.log("Conectando ao mongoDB com sucesso!");
+    });
+};
+
+connect();
+export default mongoose;
