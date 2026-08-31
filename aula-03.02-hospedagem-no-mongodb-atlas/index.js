2ed2116b7bdafb4b1acbc756f4a4e1289b3ab926
Anny Vitoria <annyvihalves3@gmail.com>
2026-08-31 14:35:07 -0300

    Conexão com atlas

--- aula-03.02-hospedagem-no-mongodb-atlas/index.js
+++ aula-03.02-hospedagem-no-mongodb-atlas/index.js
@@ -1,11 +1,14 @@
 //importando o Express
 import express, { urlencoded } from "express";
 //importando o mongoose
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 //importando o model
 import Game from "./models/Games.js";
 //importar as rotas
 import gameRoutes from "./routes/gameRoutes.js";
+
+import mongoose from './config/db.connection.js';
+
 
 //carregando o express
 const app = express();
@@ -17,7 +20,7 @@ app.use(express.urlencoded({extend : false}));
 app.use("/", gameRoutes);
 
 //iniciando a conexão com o MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/apithegames_aninhado");
+// mongoose.connect("mongodb://127.0.0.1:27017/apithegames_aninhado");
 
 //iniciando o servidor da API
 const port = 4000;
